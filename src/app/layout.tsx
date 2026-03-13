b07c2968a0d1347755dabe518e10b332deb621c6
Pedro Alves Nabozni <pedroalvesnabozni@MacBook-Air-de-Pedro.local>
2026-03-13 19:17:18 -0300

    Corrige importacao do CSS global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { StoreFooter } from "@/components/store/store-footer";
 import { StoreHeader } from "@/components/store/store-header";
